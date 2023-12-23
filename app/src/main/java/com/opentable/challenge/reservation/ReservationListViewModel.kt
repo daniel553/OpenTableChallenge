7cfacb6162966efa85f6c56ab47ab6a0d1a39d63
Daniel García <dany.el553@gmail.com>
2023-12-22 23:28:43 -0600

    Add simple state handler for all reservations

--- app/src/main/java/com/opentable/challenge/reservation/ReservationListViewModel.kt
+++ app/src/main/java/com/opentable/challenge/reservation/ReservationListViewModel.kt
@@ -1,11 +1,21 @@
 package com.opentable.challenge.reservation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.opentable.challenge.model.toListReservationItem
+import com.opentable.domain.model.Reservation
+import com.opentable.domain.usecase.GetAllReservations
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -15,15 +25,35 @@ import javax.inject.Inject
 @HiltViewModel
 class ReservationListViewModel @Inject constructor(
     //💡Space to inject the use cases.
+    private val getAllReservations: GetAllReservations
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(ReservationListState.Loading)
+    private val _uiState: MutableStateFlow<ReservationListState> =
+        MutableStateFlow(ReservationListState.Loading)
     val uiState = _uiState.asStateFlow()
 
     private val _uiEvent = MutableSharedFlow<ReservationListEvent>()
     val uiEvent = _uiEvent.asSharedFlow()
 
+    init {
+        viewModelScope.launch {
+            getAllReservations()
+                .onEach { reservations ->
+                    _uiState.update {
+                        ReservationListState.Success(reservations.toListReservationItem())
+                    }
+                }
+                .catch {
+                    _uiState.update {
+                        ReservationListState.Error
+                    }
+                }
+                .stateIn(viewModelScope)
+        }
+    }
+
     fun onEvent(event: ReservationListEvent) {
         TODO("Handle event")
     }
+
 }
