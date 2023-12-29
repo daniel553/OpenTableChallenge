538edb19d654b6129c43e32f0e7ddf28e225abe8
Daniel García <dany.el553@gmail.com>
2023-12-28 23:09:29 -0600

    Add missing state screens

--- app/src/main/java/com/opentable/challenge/reservation/ReservationListView.kt
+++ app/src/main/java/com/opentable/challenge/reservation/ReservationListView.kt
@@ -1,6 +1,14 @@
 package com.opentable.challenge.reservation
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,17 +16,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.opentable.challenge.R
@@ -121,9 +136,15 @@ fun ReservationListItemView(
     modifier: Modifier = Modifier,
     onClick: () -> Unit // Intended to be empty
 ) {
-    Column(modifier = modifier
-        .testTag(ReservationListViewTag.ReservationListItemView.name.plus(reservation.id))
-        .clickable { onClick() }) {
+    Column(
+        modifier = modifier
+            .padding(4.dp)
+            .shadow(elevation = 4.dp, clip = false, shape = RoundedCornerShape(8.dp))
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .padding(8.dp)
+            .testTag(ReservationListViewTag.ReservationListItemView.name.plus(reservation.id))
+            .clickable { onClick() }) {
         Text(
             text = reservation.name.uppercase(),
             style = MaterialTheme.typography.titleMedium
@@ -138,9 +159,32 @@ fun ReservationListItemView(
 
 @Composable
 fun ReservationListShrimmer(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        //TODO("Not yet implemented")
-        Text("Not yet implemented")
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
+    val color by infiniteTransition.animateColor(
+        initialValue = MaterialTheme.colorScheme.primaryContainer,
+        targetValue = MaterialTheme.colorScheme.inversePrimary,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "color"
+    )
+    //💡The intention is to create like a skeleton of a list, but quite simple with animation
+    LazyColumn(modifier) {
+        items(4) {
+            Box(
+                modifier = Modifier
+                    .height(62.dp)
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(1.dp)
+                    .shadow(elevation = 4.dp, clip = false, shape = RoundedCornerShape(8.dp))
+                    .drawBehind {
+                        drawRect(color)
+                    }
+            )
+        }
     }
 }
 
@@ -154,9 +198,31 @@ fun ReservationListShrimmerPreview() {
 
 @Composable
 fun ReservationListError(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        //TODO("Not yet implemented")
-        Text("Not yet implemented")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.Warning, contentDescription = "error",
+            tint = MaterialTheme.colorScheme.error,
+            modifier = Modifier
+                .width(84.dp)
+                .height(84.dp)
+                .padding(top = 16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.reservation_list_error_title),
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .padding(top = 8.dp, start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.reservation_list_error_subtitle),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 8.dp, start = 16.dp, end = 16.dp)
+        )
     }
 }
 
